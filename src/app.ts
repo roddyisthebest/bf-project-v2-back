79b431adae8e915a3b4af3d67cf0eda55fcac0ac
roddyisthebest <bsy17171@naver.com>
2022-08-03 20:08:43 +0900

    [이슈 수정] client_id 주석 제거

--- src/app.ts
+++ src/app.ts
@@ -20,20 +20,6 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
 });
 app.use('/user', userRoutes);
 
-// app.get('/auth/kakao/callback', async (req, res, next) => {
-//   const { data } = await axios.post(
-//     `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=8e90fd53b25935044191bd3ebd2bf726&&redirect_uri=http://localhost:3000/auth/kakao/callback&code=${req.query.code}`
-//   );
-
-//   const userInfo = await axios.get('https://kapi.kakao.com/v1/oidc/userinfo', {
-//     headers: {
-//       Authorization: `Bearer $${data.access_token}`,
-//     },
-//   });
-
-//   return res.send({ ...data, ...userInfo.data, message: '' });
-// });
-
 app.get('/test', authToken, async (req, res, next) => {
   console.log(req.userId);
   return res.send({ msg: 'accessToken이 유효합니다 시빠끄', id: req.userId });
